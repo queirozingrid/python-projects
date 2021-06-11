bfb873c364fbc6166e37e88bb2133b9a57b0b522
Ingrid Queiroz <queirozingrid.c@gmail.com>
2021-06-11 20:54:28 -0300

    pasta certa

---
+++ AreaCirculo/Main.py
@@ -0,0 +1,5 @@
+# programa que calcula a área do círculo após receber, pelo usuário, o valor do raio
+pi = float(3.14159)
+raio = float(input("Entre com o valor do raio\n"))
+area = float(pi*raio*raio)
+print(round(area, 2))
